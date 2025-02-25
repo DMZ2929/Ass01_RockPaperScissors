8c1eda2d091536948c0feb647497245070643fe4
DMZ2929 <dmzarins@gmail.com>
2025-02-25 00:02:56 -0500

    project completed and runs succesfully

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,71 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        boolean playAgain = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (playAgain) {
+            String playerA = "";
+            while (!playerA.equalsIgnoreCase("R") && !playerA.equalsIgnoreCase("P") && !playerA.equalsIgnoreCase("S")) {
+                System.out.print("Player A, enter your move (R/P/S): ");
+                playerA = scanner.nextLine();
+                if (!playerA.equalsIgnoreCase("R") && !playerA.equalsIgnoreCase("P") && !playerA.equalsIgnoreCase("S")) {
+                    System.out.println("Invalid move! Please enter R, P, or S.");
+                }
+            }
+
+            String playerB = "";
+            while (!playerB.equalsIgnoreCase("R") && !playerB.equalsIgnoreCase("P") && !playerB.equalsIgnoreCase("S")) {
+                System.out.print("Player B, enter your move (R/P/S): ");
+                playerB = scanner.nextLine();
+                if (!playerB.equalsIgnoreCase("R") && !playerB.equalsIgnoreCase("P") && !playerB.equalsIgnoreCase("S")) {
+                    System.out.println("Invalid move! Please enter R, P, or S.");
+                }
+            }
+
+            if (playerA.equalsIgnoreCase(playerB)) {
+                System.out.println(getMoveName(playerA) + " vs " + getMoveName(playerB) + " it's a Tie!");
+            } else if ((playerA.equalsIgnoreCase("R") && playerB.equalsIgnoreCase("S")) ||
+                    (playerA.equalsIgnoreCase("P") && playerB.equalsIgnoreCase("R")) ||
+                    (playerA.equalsIgnoreCase("S") && playerB.equalsIgnoreCase("P"))) {
+                System.out.println(getResultPhrase(playerA, playerB));
+                System.out.println("Player A wins!");
+            } else {
+                System.out.println(getResultPhrase(playerB, playerA));
+                System.out.println("Player B wins!");
+            }
+
+            System.out.print("Do you want to play again? (Y/N): ");
+            String choice = scanner.nextLine();
+            if (!choice.equalsIgnoreCase("Y")) {
+                playAgain = false;
+            }
         }
+
+        System.out.println("Thanks for playing!");
+        scanner.close();
+    }
+
+    private static String getMoveName(String move) {
+        if (move.equalsIgnoreCase("R")) {
+            return "Rock";
+        } else if (move.equalsIgnoreCase("P")) {
+            return "Paper";
+        } else if (move.equalsIgnoreCase("S")) {
+            return "Scissors";
+        }
+        return "";
+    }
+
+    private static String getResultPhrase(String winningMove, String losingMove) {
+        if (winningMove.equalsIgnoreCase("R") && losingMove.equalsIgnoreCase("S")) {
+            return "Rock breaks Scissors";
+        } else if (winningMove.equalsIgnoreCase("P") && losingMove.equalsIgnoreCase("R")) {
+            return "Paper covers Rock";
+        } else if (winningMove.equalsIgnoreCase("S") && losingMove.equalsIgnoreCase("P")) {
+            return "Scissors cuts Paper";
+        }
+        return "";
     }
 }
